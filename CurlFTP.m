79646d4cd2b7de05915e0f127ca9a8453666682f
nrj <nickrjensen@gmail.com>
2010-01-02 19:14:14 -0500

    Fixed forceReload flag for FTP directory listings.

--- CurlFTP.m
+++ CurlFTP.m
@@ -126,7 +126,7 @@ static size_t handleDirectoryList(void *ptr, size_t size, size_t nmemb, NSMutabl
 	
 	NSMutableArray *list;
 	
-	if (forceReload || !(list = [directoryListCache objectForKey:url]))
+	if (reload || !(list = [directoryListCache objectForKey:url]))
 	{
 		list = [[NSMutableArray alloc] init];
 		
